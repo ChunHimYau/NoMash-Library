5a3877a5c7de50b99aab8240757b1688e3da031c
cyau0010 <cyau0010@student.monash.edu>
2025-08-28 20:21:38 +1000

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './views/HomeView.vue'
 </script>
 
 <template>
